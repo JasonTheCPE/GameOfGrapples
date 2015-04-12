84a48195f409521145108c0b43d5c567380e21a2
aquira246 <aacost06@calpoly.edu>
2015-04-12 01:16:40 -0700

    bullets are in networking

---
+++ Assets/Scripts/Netoworking/BulletScript.js
@@ -0,0 +1,12 @@
+﻿#pragma strict
+var bulletspeed = 5;
+
+function Update () {
+	var direction:Vector3 = Vector3(0, bulletspeed * Time.deltaTime, 0);
+	GetComponent(NetworkView).RPC("Move", RPCMode.AllBuffered, direction);
+}
+
+@RPC
+function Move(dir:Vector3) {
+	GetComponent(CharacterController).Move(dir);
+}
--- Assets/Scripts/Netoworking/MovementScript.js
+++ Assets/Scripts/Netoworking/MovementScript.js
@@ -2,6 +2,8 @@
 var gravity = 5;
 private var cc: CharacterController;
 
+var bulletPrefab:GameObject;
+
 function Start() {
 	cc = GetComponent(CharacterController);
 }
@@ -9,7 +11,29 @@ function Start() {
 function Update(){
 	if (GetComponent(NetworkView).isMine) {
 		cc.Move(Vector3(Input.GetAxis("Horizontal") * speed * Time.deltaTime, -gravity * Time.deltaTime, Input.GetAxis("Vertical") * speed * Time.deltaTime));
+		if(Input.GetMouseButtonDown(0)){
+            GetComponent(NetworkView).RPC("shoot", RPCMode.All);
+    	}
 	} else {
 		enabled = false;
 	}
+}
+
+@RPC
+function shoot () {
+	Network.Instantiate(bulletPrefab, transform.position, Quaternion.identity, 0);
+    /*if (shotSound) audio.PlayOneShot(shotSound); // play the shot sound
+    var hit: RaycastHit;
+    if (Physics.Raycast(transform.position, transform.forward, hit))
+	{
+        // prepare rotation to instantiate blood or sparks
+        var rot = Quaternion.FromToRotation(Vector3.up, hit.normal);
+        if (hit.transform.tag == "Player"){ // if enemy hit...
+            if (bloodPrefab) Instantiate(bloodPrefab, hit.point, rot); // make it bleed...
+            //hit.transform.SendMessage("ApplyDamage", 5, SendMessageOptions.DontRequireReceiver); // and consume its health
+            hit.transform.networkView.RPC("ApplyDamage", RPCMode.All, 5);  
+        } else { // otherwise emit sparks at the hit point
+            if (sparksPrefab) Instantiate(sparksPrefab, hit.point, rot);
+        }
+    }*/
 }
