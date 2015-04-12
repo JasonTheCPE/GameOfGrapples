00b893140ab9981cef95c29cbac886c16450b2f7
JasonTheCPE <JasonTheCPE@gmail.com>
2015-04-11 23:48:28 -0700

    Initial Commit!

---
+++ Assets/Scripts/SceneTransition.cs
@@ -0,0 +1,13 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class SceneTransition : MonoBehaviour
+{
+	//public GameObject loadingImage;
+	
+	public void LoadScene(string sceneName)
+	{
+		//loadingImage.SetActive(true);
+		Application.LoadLevel(sceneName);
+	}
+}
---
+++ README.md
@@ -0,0 +1,3 @@
+# GameOfGrapples
+Repository of the unity code for Jason Swanson and Andrew Acosta's "Game of Grapples" game project for CPE 400 taken at the California Polytechnic Univerisity of San Luis Obispo.
+
