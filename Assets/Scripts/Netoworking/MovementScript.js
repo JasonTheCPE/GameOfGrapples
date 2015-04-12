caacbff1ed7d3c796b519709dafd8e6e8d50cd47
aquira246 <aacost06@calpoly.edu>
2015-04-12 01:23:43 -0700

    improved code a little for networking

--- Assets/Scripts/Netoworking/MovementScript.js
+++ Assets/Scripts/Netoworking/MovementScript.js
@@ -21,19 +21,7 @@ function Update(){
 
 @RPC
 function shoot () {
-	Network.Instantiate(bulletPrefab, transform.position, Quaternion.identity, 0);
-    /*if (shotSound) audio.PlayOneShot(shotSound); // play the shot sound
-    var hit: RaycastHit;
-    if (Physics.Raycast(transform.position, transform.forward, hit))
-	{
-        // prepare rotation to instantiate blood or sparks
-        var rot = Quaternion.FromToRotation(Vector3.up, hit.normal);
-        if (hit.transform.tag == "Player"){ // if enemy hit...
-            if (bloodPrefab) Instantiate(bloodPrefab, hit.point, rot); // make it bleed...
-            //hit.transform.SendMessage("ApplyDamage", 5, SendMessageOptions.DontRequireReceiver); // and consume its health
-            hit.transform.networkView.RPC("ApplyDamage", RPCMode.All, 5);  
-        } else { // otherwise emit sparks at the hit point
-            if (sparksPrefab) Instantiate(sparksPrefab, hit.point, rot);
-        }
-    }*/
+	if (GetComponent(NetworkView).isMine) {
+		Network.Instantiate(bulletPrefab, transform.position + Vector3(0,1.5,0), Quaternion.identity, 0);
+	}
 }
