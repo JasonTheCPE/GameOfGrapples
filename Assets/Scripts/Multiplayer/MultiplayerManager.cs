dabf0e0c02fb9fadcf087cd384b51f27a8efcd49
aquira246 <aacost06@calpoly.edu>
2015-05-16 18:28:56 -0700

    Fixed most networking issues I can find? Pretty much the rope is nice, and so is the kunai :D

--- Assets/Scripts/Multiplayer/MultiplayerManager.cs
+++ Assets/Scripts/Multiplayer/MultiplayerManager.cs
@@ -14,7 +14,7 @@ public class MultiplayerManager : MonoBehaviour {
 	private bool isCustom = false;
 	private Object[] skins;
 	private int usingSkin = 0;
-	private int matchTime = 0;
+	public int matchTime = 60, matchHP = 1;
 	
 	public string playerName = "Host Player";	//the player name the current player will have in the match
 	public GameObject playerPrefab;
@@ -226,6 +226,7 @@ public class MultiplayerManager : MonoBehaviour {
 				GameObject.Find("Ingame Manager").GetComponent<Referee>().isTimed = true;
 			}
 			GameObject.Find("Ingame Manager").GetComponent<Referee>().timer = matchTime;
+			GameObject.Find("Ingame Manager").GetComponent<Referee>().startHealth = matchHP;
 			spawnPlayer(usingSkin);
 		} else if (level == 5) {
 			if (Network.isServer) {
@@ -272,6 +273,8 @@ public class MultiplayerManager : MonoBehaviour {
 		}
 		myPlayerGO.GetComponent<HoverName>().name = PlayerList[playerNumber].playerName;			//sets the name of the person?
 		myPlayerGO.GetComponent<HoverName>().localName = PlayerList[playerNumber].playerName;
+
+		myPlayerGO.GetComponent<PlayerMovement>().health = matchHP;
 	}
 	
 	GameObject accessSkin(int skinID) {
@@ -303,6 +306,11 @@ public class MultiplayerManager : MonoBehaviour {
 	[RPC]
 	void SetTime(int time) {
 		matchTime = time;
+	}
+
+	[RPC]
+	void SetHealth(int hp) {
+		matchHP = hp;
 	}
 	
 }
--- Assets/Scripts/Player Scripts/Projectiles/KunaiInnerCollide.cs
+++ Assets/Scripts/Player Scripts/Projectiles/KunaiInnerCollide.cs
@@ -7,9 +7,7 @@ public class KunaiInnerCollide : MonoBehaviour
 	{
 		if(other.tag == "Tiles" && GetComponentInParent<Kunai>().beingRetracted == false)
 		{
-			GetComponentInParent<Kunai>().isStuck = true;
-			GetComponentInParent<Kunai>().turnTowardsVelocity = false;
-			GetComponentInParent<Rigidbody2D>().isKinematic = true;
+			GetComponentInParent<Kunai>().GetComponent<NetworkView>().RPC("GetStuck", RPCMode.All);
 		}
 		//Debug.Log("Kunai inner collider triggered: " + other.tag);
 	}
