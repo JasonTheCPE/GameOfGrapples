a7d6a6ec0893cbfa6551bfb9126fb1214c8622dc
JasonTheCPE <JasonTheCPE@gmail.com>
2015-05-18 13:01:51 -0700

    Player now has health and is damageable.

---
+++ Assets/Scripts/Player Scripts/DamagableChild.cs
@@ -0,0 +1,17 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class DamagableChild : MonoBehaviour
+{
+	Health health;
+	// Use this for initialization
+	void Start ()
+	{
+		health = GetComponentInParent<Health>();
+	}
+	
+	public void TakeDamage()
+	{
+		health.GetHurt();
+	}
+}
